d4caecfa95fbf6fa14b31c674ebe8b2654e3eace
pedago <pedago@pcalb-mm0406.univ-jfc.fr>
2018-09-26 16:08:43 +0200

    premier commit

---
+++ src/main/java/com/mycompany/bowlingproject/Multiplayer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bowlingproject;
+
+import bowling.MultiPlayerGame;
+import bowling.SinglePlayerGame;
+
+/**
+ *
+ * @author pedago
+ */
+public class Multiplayer implements MultiPlayerGame {
+
+    @Override
+    public String startNewGame(String[] playerName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String lancer(int nombreDeQuillesAbattues) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int scoreFor(String playerName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
